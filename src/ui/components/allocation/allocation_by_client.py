d6959f32e5a24e9af7c8b8ad6e4ebbe43c074c97
Elemar Rodrigues Severo Junior <me@elemarjr.com>
2024-08-21 08:46:28 -0300

    Refactor graph generation logic into a separate module
    This commit moves the code for generating graphs from individual component files to a shared `timesheet_field_graph` module. This change improves code reusability and maintainability by centralizing the graph rendering logic. The affected components include allocation by client, worker, and product or service.

--- src/ui/components/allocation/allocation_by_client.py
+++ src/ui/components/allocation/allocation_by_client.py
@@ -1,11 +1,12 @@
-from dash import dcc, html
-import plotly.graph_objs as go
+from dash import html
 import pandas as pd
 import dash_bootstrap_components as dbc
+
 import ui.components.base.cards as c
-import ui.components.base.colors as colors
+import ui.components.allocation.timesheet_field_graph as tfg
 
 from models.datasets.timesheet_dataset import TimeSheetFieldSummary
+
 
 def render(data: pd.DataFrame):
     if len(data) == 0:
@@ -13,55 +14,15 @@ def render(data: pd.DataFrame):
 
     tfs = TimeSheetFieldSummary(data, 'ClientName')
 
-    columns = ['Squad', 'Consulting', 'Internal']
-
     return html.Div(
         [
-            dbc.Row([
-                c.create_kpi_card('Unique Clients', tfs.unique, 4),
-                c.create_kpi_card('Avg. Hours per Client', f'{tfs.avg_hours:.1f} hrs', 4),
-                c.create_kpi_card('Std. Hours per Client', f'{tfs.std_hours:.1f}', 4),
-            ], style={'marginBottom': '10px'}),
             dbc.Row(
                 [
-                    dbc.Col(
-                        c.create_graph_card(
-                            "Hours Allocated by Client and Work Type",
-                            dcc.Graph(
-                                figure={
-                                    'data': [
-                                                go.Bar(
-                                                    x=tfs.grouped_total_hours.index,
-                                                    y=tfs.grouped_total_hours[kind],
-                                                    name=kind,
-                                                    marker_color=colors.KIND_COLORS[kind]
-                                                ) for kind in columns if kind in tfs.grouped_total_hours
-                                            ] + [
-                                                go.Scatter(
-                                                    x=[tfs.allocation_80, tfs.allocation_80],
-                                                    y=[0, tfs.grouped_total_hours['Total'].max()],
-                                                    mode='lines',
-                                                    name='80% Allocation',
-                                                    line=dict(color='red', dash='dash')
-                                                )
-                                            ],
-                                    'layout': go.Layout(
-                                        barmode='stack',
-                                        xaxis={'title': 'Clients'},
-                                        yaxis={'title': 'Hours Worked'},
-                                        showlegend=True,
-                                        legend={'orientation': 'h', 'y': -0.2},
-                                        plot_bgcolor='rgba(0,0,0,0)',
-                                        paper_bgcolor='rgba(0,0,0,0)',
-                                        font_color='white',
-                                        margin=dict(l=50, r=20, t=20, b=100),
-                                        height=400
-                                    )
-                                }
-                            )
-                        ), width=12
-                    )
-                ]
-            )
+                    c.create_kpi_card('Unique Clients', tfs.unique, 4),
+                    c.create_kpi_card('Avg. Hours per Client', f'{tfs.avg_hours:.1f} hrs', 4),
+                    c.create_kpi_card('Std. Hours per Client', f'{tfs.std_hours:.1f}', 4),
+                ], style={'marginBottom': '10px'}
+            ),
+            dbc.Row(tfg.render(tfs, 'Hours Allocated by Client and Work Type', 'Client'))
         ]
     )
--- src/ui/components/allocation/allocation_by_product_or_service.py
+++ src/ui/components/allocation/allocation_by_product_or_service.py
@@ -6,6 +6,7 @@ import ui.components.base.colors as colors
 import ui.components.base.cards as c
 
 from models.datasets.timesheet_dataset import TimeSheetFieldSummary
+import ui.components.allocation.timesheet_field_graph as tfg
 
 
 def render(data: pd.DataFrame):
@@ -20,46 +21,6 @@ def render(data: pd.DataFrame):
                 c.create_kpi_card('Avg. Hours per POS', f'{tfs.avg_hours:.1f} hrs', 4),
                 c.create_kpi_card('Std. Hours per POS', f'{tfs.std_hours:.1f}', 4),
             ], style={'marginBottom': '10px'}),
-            dbc.Row(
-                [
-                    dbc.Col(
-                        c.create_graph_card(
-                            "All POS by Hours",
-                            dcc.Graph(
-                                figure={
-                                    'data': [
-                                                go.Bar(
-                                                    x=tfs.grouped_total_hours.index,
-                                                    y=tfs.grouped_total_hours[kind],
-                                                    name=kind,
-                                                    marker_color=colors.KIND_COLORS[kind]
-                                                ) for kind in tfs.grouped_total_hours.columns[:-1]
-                                            ] + [
-                                                go.Scatter(
-                                                    x=[tfs.allocation_80, tfs.allocation_80],
-                                                    y=[0, tfs.grouped_total_hours['Total'].max()],
-                                                    mode='lines',
-                                                    name='80% Allocation',
-                                                    line=dict(color='red', dash='dash')
-                                                )
-                                            ],
-                                    'layout': go.Layout(
-                                        barmode='stack',
-                                        xaxis={'title': 'POS', 'tickangle': 45},
-                                        yaxis={'title': 'Hours'},
-                                        showlegend=True,
-                                        legend={'orientation': 'h', 'y': -0.2},
-                                        plot_bgcolor='rgba(0,0,0,0)',
-                                        paper_bgcolor='rgba(0,0,0,0)',
-                                        font_color='white',
-                                        margin=dict(l=50, r=20, t=20, b=100),
-                                        height=500
-                                    )
-                                }
-                            )
-                        ), width=12
-                    )
-                ]
-            ),
+            dbc.Row(tfg.render(tfs, 'Hours by Products or Services', 'Products or Services'))
         ]
     )
